3042520c757e9018cc5bc866cee652eb215bd33f
Pablo Gago <pablure@gmail.com>
2022-05-05 20:18:50 +0200

    Ejemplo Samuel

---
+++ src/views/Blog.vue
@@ -0,0 +1,16 @@
+<template>
+    <Titulo texto="Título de mi blog"/>
+</template>
+
+<script>
+import Titulo from '.. /components/Titulo'
+export default {
+    component: {
+        Titulo
+    }
+}
+</script>
+
+<style>
+
+</style>
